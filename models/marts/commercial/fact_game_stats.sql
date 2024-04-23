70a23a81fee414b529e371e929bba5109b127470
Anand <165329816+andyDE0328@users.noreply.github.com>
2024-04-23 14:29:04 -0400

    Var changes

--- models/marts/commercial/fact_game_stats.sql
+++ models/marts/commercial/fact_game_stats.sql
@@ -1,8 +1,9 @@
 with
     game_wide as (select * from {{ ref("int_game_wide") }}),
     schedule as (select * from {{ ref("stg_schedule") }}),
-    final as (select t1.*, t2.status
-                from game_wide t1
+    final as (select t1.*, t2.status,
+                t1.avg_duration > {{ var('std_time')}} as is_over_time
+              from game_wide t1
                         inner join
                     schedule t2
                         on t1.gameid = t2.gameid)
