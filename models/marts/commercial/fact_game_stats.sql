51699d7295761ca8fc48f9fbb5b9179a7162a14a
Anand <165329816+andyDE0328@users.noreply.github.com>
2024-04-23 16:30:32 -0400

    dbt Artifact

--- models/marts/commercial/fact_game_stats.sql
+++ models/marts/commercial/fact_game_stats.sql
@@ -1,5 +1,5 @@
 {{ config(
-    pre_hook="insert into `project-beyond-418616.stagging.model_logging` values ('fact_game_stats','started', CURRENT_TIMESTAMP())" ,
+    pre_hook="insert into `project-beyond-418616.stagging.model_logging` values ('fact_game_stats','Started', CURRENT_TIMESTAMP())" ,
     post_hook="insert into `project-beyond-418616.stagging.model_logging` values ('fact_game_stats','Ended', CURRENT_TIMESTAMP())" ,
 ) }}
 
